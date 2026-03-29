5ce017a45bd376736162075f103218da94e0c6de
Suhaas Goluguri <suhaas.goluguri@gmail.com>
2026-03-28 23:19:30 -0400

    added deque for access, capped at 30 fps

---
+++ algorithm.py
@@ -0,0 +1,6 @@
+import numpy as np
+import cv2
+from collections import deque
+import screen_capture
+frames = screen_capture.frame_lists
+
--- screen_capture.py
+++ screen_capture.py
@@ -3,33 +3,37 @@ import numpy as np
 from mss import tools
 import cv2
 import threading
-import multiprocessing
-from multiprocessing import shared_memory
+import time
+import collections
+
 
 sct = mss.mss()
 running = False
+frame_lists = collections.deque(maxlen=30)
+target_frames = 30
+target_frame_time = 1/target_frames
 
 def start_capture():
     global running
     running = True
-    thread = threading.Thread(target=run_loop())
+    thread = threading.Thread(target=run_loop)
     thread.start()
 
 def stop_capture():
     global running
     running = False
 
-def frame_capture():
-    with sct:
-
-        img = sct.grab(sct.monitors[1])
-
-        img_np = np.array(img)
-
-        resized = cv2.resize(img_np, (320, 180))
-        return resized
-def run_loop():   
-    while running:
-        frame = frame_capture()
+def run_loop():
+    global running, frame_lists, target_frame_time
+    with mss.mss() as sct:   
+        while running:
+            iteration_start = time.time()
+            img = sct.grab(sct.monitors[1])
+            img_np = np.array(img)
+            resized = cv2.resize(img_np, (320, 180))
+            frame_lists.append(resized)
+            iteration_end = time.time()
+            sleep_time = max(target_frame_time - (iteration_end - iteration_start), 0)
+            time.sleep(sleep_time)
             
 
