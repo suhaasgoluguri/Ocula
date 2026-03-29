80ea928d9c963d2fb755e2609f4d06d5490b3ca1
Suhaas Goluguri <suhaas.goluguri@gmail.com>
2026-03-29 00:10:28 -0400

    added relative luminance to algorithm

--- algorithm.py
+++ algorithm.py
@@ -2,5 +2,26 @@ import numpy as np
 import cv2
 from collections import deque
 import screen_capture
-frames = screen_capture.frame_lists
+
+frame_deque = screen_capture.frame_list
+
+
+def relative_luminance():
+    frames = np.array(list(frame_deque))
+    if len(frames) < 30:
+        return None
+
+    #chunk into 144 chunks per frame
+    chunked = frames.reshape(30, 9, 20, 16, 20, 3).transpose(0, 1, 3, 2, 4, 5).reshape(30, 144, 20, 20, 3)
+    normalized = chunked/255
+    mask = normalized <= 0.03928
+    linearized = np.where(mask, normalized/12.92, ((normalized+0.055)/1.055)**2.4) 
+    
+    red = linearized[:, :, :, 0]
+    green = linearized[:, :, :, 1]
+    blue = linearized[:, :, :, 2]
+
+    relative_luminance = 0.2126 * red + 0.7152 * green + 0.0722 * blue
+    return relative_luminance
+
 
--- screen_capture.py
+++ screen_capture.py
@@ -7,15 +7,15 @@ import time
 import collections
 
 
-sct = mss.mss()
 running = False
-frame_lists = collections.deque(maxlen=30)
+frame_list = collections.deque(maxlen=30)
 target_frames = 30
 target_frame_time = 1/target_frames
 
 def start_capture():
     global running
     running = True
+
     thread = threading.Thread(target=run_loop)
     thread.start()
 
@@ -24,14 +24,18 @@ def stop_capture():
     running = False
 
 def run_loop():
-    global running, frame_lists, target_frame_time
+    global running, frame_list, target_frame_time
     with mss.mss() as sct:   
         while running:
             iteration_start = time.time()
-            img = sct.grab(sct.monitors[1])
-            img_np = np.array(img)
-            resized = cv2.resize(img_np, (320, 180))
-            frame_lists.append(resized)
+            image = sct.grab(sct.monitors[1])
+
+            np_image = np.array(image)
+            rgb = cv2.cvtColor(np_image, cv2.COLOR_BGRA2RGB)
+            resized = cv2.resize(rgb, (320, 180))
+
+            frame_list.append(resized)
+
             iteration_end = time.time()
             sleep_time = max(target_frame_time - (iteration_end - iteration_start), 0)
             time.sleep(sleep_time)
